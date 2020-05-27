ccbabc5f615ccd487ef0540584ba50ae879901da
Natasha Rios <natu.rios@hotmail.com>
2020-05-27 18:17:04 -0300

    Hice el menu hamburguesa para mobile

--- js/index.js
+++ js/index.js
@@ -2,5 +2,10 @@ const menuOn = document.querySelector(".js-menuOn");
 const listaNav = document.querySelector(".js-listaNav");
 
 menuOn.onclick = function (event) {
+    if(listaNav.style.display == "none") {
     listaNav.style.display = "block";
+    }else {
+        listaNav.style.display = "none"
+    }
 }
+
