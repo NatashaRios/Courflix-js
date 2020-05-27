64751456cc88e764526fe9c9c01cdd049f6e49ae
Natasha Rios <natu.rios@hotmail.com>
2020-05-26 21:48:50 -0300

    Segui con la pagina de detalles

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const menuOn = document.querySelector(".js-menuOn");
+const listaNav = document.querySelector(".js-listaNav");
+
+menuOn.onclick = function (event) {
+    listaNav.style.display = "block";
+}
