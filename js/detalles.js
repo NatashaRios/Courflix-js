129906cffaaf73f3957a1d39456e026b376346e4
Natasha Rios <natu.rios@hotmail.com>
2020-05-28 23:42:45 -0300

    Agrego reset, corrijo los tamaños y carrousel

--- js/detalles.js
+++ js/detalles.js
@@ -1,1 +1,65 @@
+ $(document).ready(function(){
+    $('.capitulosTemporada').slick({
+      infinite: false,
+      arrows: true,
+      slidesToShow: 4,
+      slidesToScroll: 3,
 
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: "unslick"
+        }
+      ]
+    })   
+    });
+  
+
+/* $slick_slider = $('.capitulosTemporada');
+  settings = {
+    // some settings
+    infinite: false,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 3
+  }
+  $slick_slider.slick(settings);
+
+  // reslick only if it's not slick()
+  $(window).on('resize', function() {
+    if ($(window).width() < 768) {
+      if ($slick_slider.hasClass('slick-initialized')) {
+        $slick_slider.slick('unslick');
+      }
+      return
+    }
+
+    if (!$slick_slider.hasClass('slick-initialized')) {
+      return $slick_slider.slick(settings);
+    }
+  }); */
+
+ /*  function slickCarrousel() {
+    $(".capitulosTemporada").slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 3,
+      variableWidth: true,
+      accesibility: true,
+      arrows: true,
+      responsive: [
+      {
+        breakpoint: 600,
+        settings: "unslick"
+      },
+     ]
+    })
+  };
+  
+  slickCarrousel()
+  
+  $(window).resize(function(){
+    if ($(window).width() > 768) {
+        slickCarrousel();
+    }
+  });  */
--- js/index.js
+++ js/index.js
@@ -9,3 +9,60 @@ menuOn.onclick = function (event) {
     }
 }
 
+ $(document).ready(function(){
+    $('.series').slick({
+      infinite: true,
+      arrows: true,
+      slidesToShow: 4,
+      slidesToScroll: 3,
+    
+      responsive: [
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    })   
+
+});
+
+$(document).ready(function(){
+    $('.seguirViendo').slick({
+      infinite: true,
+      arrows: true,
+      slidesToShow: 4,
+      slidesToScroll: 3,
+    
+      responsive: [
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    })   
+});
+
+
+    $(document).ready(function(){
+        $('.agregadosRecientemente').slick({
+          infinite: true,
+          arrows: true,
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          responsive: [
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        })   
+    });
